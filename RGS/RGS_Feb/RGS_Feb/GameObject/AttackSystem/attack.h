d06d46ffa403e1f64d28f8dbd852c493de894308
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-02-05 17:41:54 +0900

    CollisionBoxのbug修正 AttackとAttackManagerを完成

--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack.h
@@ -5,19 +5,23 @@
 //-------------------------------------------------------
 #pragma once
 #include <Math\vector3.h>
+#include <Math\collision_box.h>
 
 namespace AttackSystem
 {
 	class Attack
 	{
 	public:
-		Attack();
 		virtual void Update() = 0;
 		virtual void Draw() = 0;
 		bool IsEnd() { return m_is_end; }
+		Math::CollisionBox GetBox()
+		{
+			return Math::CollisionBox(m_position - m_size / 2, m_position + m_size / 2);
+		}
 	private:
-		short timer;		//単位：frame
 		bool m_is_end;
 		Math::Vector3 m_position;		//CollisionBoxの中心位置
+		Math::Vector3 m_size;			//CollisionBoxの大きさ
 	};
 }
--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack_manager.cpp
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack_manager.cpp
@@ -21,3 +21,34 @@ void AttackManager::Initialize()
 {
 	m_atk_list.clear();
 }
+
+void AttackManager::AddAttack(std::shared_ptr<Attack> atk)
+{
+	m_add_list.push_back(atk);
+}
+
+void AttackManager::AddAttack()
+{
+	for (auto atk : m_add_list)
+	{
+		m_atk_list.push_back(atk);
+	}
+}
+
+void AttackManager::Update()
+{
+	AddAttack();
+
+	for (auto atk : m_atk_list)
+	{
+		atk->Update();
+	}
+}
+
+void AttackManager::Draw()
+{
+	for (auto atk : m_atk_list)
+	{
+		atk->Draw();
+	}
+}
