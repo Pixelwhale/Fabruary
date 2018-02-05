5a623c23527b65c90e39e07549866d90c525c526
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-02-05 16:07:49 +0900

    AttackSystem作成

--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack_manager.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack_manager.h
@@ -1,9 +1,30 @@
+//-------------------------------------------------------
+// 作成者：廖啓勝
+// 作成日：2017.02.05
+// 内容：Attackの更新と描画
+//-------------------------------------------------------
 #pragma once
+#include "attack.h"
+#include <Math\collision_box.h>
+#include <memory>
+#include <vector>
 
-namespace AttackManager
+namespace AttackSystem
 {
 	class AttackManager
 	{
+	public:
+		AttackManager();
+		~AttackManager();
+		void Initialize();
 
+		void AddAttack(std::shared_ptr<Attack>);
+		void Update();
+		void Draw();
+	private:
+		void AddAttack();
+
+		std::vector<std::shared_ptr<Attack>> m_atk_list;
+		std::vector<std::shared_ptr<Attack>> m_add_list;
 	};
 }
