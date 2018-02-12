f4f07323de98ed429b5959d985b3c3179abab95a
Kimsunwon <flypig1208@gmail.com>
2018-02-12 15:36:16 +0900

    CharacterBaseの移動、攻撃、描画の作成

--- RGS/RGS_Feb/RGS_Feb/Character/character_base.h
+++ RGS/RGS_Feb/RGS_Feb/Character/character_base.h
@@ -13,18 +13,27 @@ namespace Character
 	class CharacterBase
 	{
 	public:
-		CharacterBase(Math::Vector3 velocity);
-		~CharacterBase();
-		void Initialize();
-		void Update();
-		void MoveUpdate();
-		void GageUpdate();
+		CharacterBase();		//コンストラクタ
+		~CharacterBase();		//デストラクタ
+		void Initialize();		//初期化
+		void Update();			//更新
+		void Attack();			//攻撃
+		void MoveUpdate();		//移動更新
+		void GageUpdate();		//ゲージ更新
+		void Draw();			//描画
 	private:
-		int m_hp;
+		int m_hp;				
 		int m_gage;
+		int m_atk_state;		//攻撃状態
+		int m_atk_cnt;			//技を打つカウント
+		bool m_isDead;
+		bool m_isJump;
 		Math::Vector3 m_position;
 		Math::Vector3 m_velocity;
+		Math::Vector3 m_size;
+		Math::Vector3 m_rotation;
 		std::shared_ptr<Core::InputState> m_input;
+		std::shared_ptr<Device::Renderer> m_renderer;
 	};
 
 
