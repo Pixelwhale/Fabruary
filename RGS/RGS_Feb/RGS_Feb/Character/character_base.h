c284b8a3d433e203d3afbfb09b9170ac84b70384
Kimsunwon <flypig1208@gmail.com>
2018-02-19 10:51:19 +0900

    CharacterBaseの修正（バグ直しとメソッドの修正）

--- RGS/RGS_Feb/RGS_Feb/Character/character_base.h
+++ RGS/RGS_Feb/RGS_Feb/Character/character_base.h
@@ -15,37 +15,44 @@ namespace Character
 	class CharacterBase
 	{
 	public:
-		CharacterBase();		//コンストラクタ
-		CharacterBase(Math::Vector3 position,Side side);
-		~CharacterBase();		//デストラクタ
-		void Initialize();		//初期化
-		void Update();			//更新
-		void Hit(CharacterBase* character);				//あたり判定
-		bool Collision(CharacterBase* other);//当たったか？
-		void Attack();			//攻撃
-		void MoveUpdate();		//移動更新
-		void GageUpdate();		//ゲージ更新
-		Side GetSide();		//チームの取得
-		void SetSide(Side side);//チームの設定
-		int GetHp();			//Hpの取得
-		int GetMp();			//Mpの取得
+		//コンストラクタ（位置、チーム、ID、Hp）
+		CharacterBase(Math::Vector3 position,Side side,int id,int hp);
+		~CharacterBase();			//デストラクタ
+		void Initialize(Math::Vector3 position, int hp);//初期化
+		void Update();				//更新
+		void Collide();				//あたり判定
+		void Attack();				//攻撃
+		void Motion();				//モーション
+
+	public:							//取得関連
+		Side	GetSide();			//チームの取得
+		int		GetHp();			//Hpの取得
+		int		GetMp();			//Mpの取得
 		Math::Vector3 GetPosition();//位置の取得
-		void SetPosition(Math::Vector3 position);//位置の設定
-		bool IsRight();			//向きを返す
-		void DirectionUpdate();	//向きの更新
-		void Motion();			//モーション
-		bool IsDead();			//死んだか？
+		void	SetPosition(Math::Vector3 position);//位置の設定
+		bool	IsRight();			//向きを返す
+		bool	IsDead();			//死んだか？
+		Math::CollisionBox GetBox()
+		{
+			return CollisionBox(m_position - Vector3(Size::kCharaX / 2, Size::kCharaY / 2, Size::kCharaZ / 2), m_position + Vector3(Size::kCharaX / 2, Size::kCharaY / 2, Size::kCharaZ / 2));
+		}
+
+	private:						//更新関連
+		void MoveUpdate();			//移動更新
+		void GageUpdate();			//ゲージ更新
+		void DirectionUpdate();		//向きの更新
 
 	private:
-		int m_hp;				
-		int m_mp;
-		int m_atk_state;		//攻撃状態
-		int m_atk_cnt;			//技を打つカウント
-		float m_speed;
-		bool m_isDead;
-		bool m_isJump;
-		bool m_isRight;
-		Side m_side;
+		int		m_hp;				
+		int		m_mp;
+		int		m_atk_state;		//攻撃入力状態
+		int		m_atk_cnt;			//連続入力カウント
+		float	m_speed;
+		bool	m_isDead;
+		bool	m_isJump;
+		bool	m_isRight;
+		int		m_id;
+		Side	m_side;
 		Math::Vector3 m_position;
 		Math::Vector3 m_velocity;
 		Math::Vector3 m_size;
@@ -54,7 +61,6 @@ namespace Character
 		std::shared_ptr<Device::Renderer> m_renderer;
 		
 	protected:
-		//Job::JobBase m_job;
 	};
 
 
--- RGS/RGS_Feb/RGS_Feb/Character/character_manager.h
+++ RGS/RGS_Feb/RGS_Feb/Character/character_manager.h
@@ -6,6 +6,7 @@
 #pragma once
 #include <Device\game_device.h>
 #include<Character\character_base.h>
+#include<Math\vector3.h>
 
 namespace Character
 {
@@ -14,17 +15,16 @@ namespace Character
 	public:
 		CharacterManager();					//コンストラクタ
 		void Initialize();					//初期化
-		void Add(CharacterBase* character);	//オブジェクトの追加
-		void HitToCharacter();				//オブジェクト同士の衝突
+		void Add(Math::Vector3 position, Side side, int hp);	//オブジェクトの追加
+		void Collide();						//オブジェクト同士の衝突
 		void RemoveDeadCharacters();		//死亡キャラを削除
 		void Update();						//更新
 		void Motion();						//モーション
-		void AddCharacter(CharacterBase* character);//モーション
-
 
 	private:
-		std::vector<CharacterBase*> m_character_list;	//キャラクターリスト
-		std::vector<CharacterBase*> m_add_characters;	//追加キャラクター
+		std::vector<std::shared_ptr<CharacterBase>> m_character_list;	//キャラクターリスト
+		std::vector<std::shared_ptr<CharacterBase>> m_add_characters;	//追加キャラクター
 
+		static int m_id;
 	};
 }
