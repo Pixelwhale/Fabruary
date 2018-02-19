266a9d924176ca241a45e230b86a6cf4b5037b27
Adrian Rhesa <adrian.rhesa.ss@gmail.com>
2018-02-19 17:05:25 +0900

    JobBaseのVirtualMethodはリファレンスからshared_ptrに変更された。

--- RGS/RGS_Feb/RGS_Feb/GameObject/Job/job_base.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/Job/job_base.h
@@ -17,12 +17,12 @@ namespace Job
 	class JobBase
 	{
 	public:
-		virtual std::string Punch(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right) = 0;
-		virtual std::string Kick(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right) = 0;
-		virtual std::string Skill1(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right) = 0;
-		virtual std::string Skill2(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right) = 0;
-		virtual std::string Skill3(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right) = 0;
-		virtual std::string Skill4(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right) = 0;
+		virtual std::string Punch(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right) = 0;
+		virtual std::string Kick(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right) = 0;
+		virtual std::string Skill1(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right) = 0;
+		virtual std::string Skill2(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right) = 0;
+		virtual std::string Skill3(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right) = 0;
+		virtual std::string Skill4(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right) = 0;
 		virtual void Update() = 0;
 	};
 }
--- RGS/RGS_Feb/RGS_Feb/GameObject/Job/programmer.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/Job/programmer.h
@@ -15,12 +15,12 @@ namespace Job
 	public :
 		Programmer(Side side);
 		~Programmer();
-		virtual std::string Punch(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right);
-		virtual std::string Kick(AttackSystem::AttackManager &attack_manager, Math::Vector3 position,bool is_right);
-		virtual std::string Skill1(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right);
-		virtual std::string Skill2(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right);
-		virtual std::string Skill3(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right);
-		virtual std::string Skill4(AttackSystem::AttackManager &attack_manager, Math::Vector3 position, bool is_right);
+		virtual std::string Punch(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right);
+		virtual std::string Kick(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position,bool is_right);
+		virtual std::string Skill1(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right);
+		virtual std::string Skill2(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right);
+		virtual std::string Skill3(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right);
+		virtual std::string Skill4(std::shared_ptr<AttackSystem::AttackManager> attack_manager, Math::Vector3 position, bool is_right);
 		virtual void Update();
 
 	private :
