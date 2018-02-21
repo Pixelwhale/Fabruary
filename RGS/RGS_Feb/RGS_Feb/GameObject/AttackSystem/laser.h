f748fbfbe4b298f7c9370474ef5302e3ed35952b
Adrian Rhesa <adrian.rhesa.ss@gmail.com>
2018-02-21 12:05:36 +0900

    Laser タイプのスキルを追加する。

--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/laser.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/laser.h
@@ -13,8 +13,17 @@ namespace AttackSystem
 	class Laser : public Attack
 	{
 	public :
-		///<summary> </summary>
-		Laser(Math::Vector3 position, Math::Vector3 size, Side side, int attack, int knockdown, int knockback, float life_span_timer);
+		///<summary>コンストラクタ</summary>
+		///<param name = "first position">最初の位置</param>
+		///<param name = "size">スキルのサイズ</param>
+		///<param name = "side">サイド</param>
+		///<param name = "attack">攻撃ポイント</param>
+		///<param name = "knockdown">敵を倒せられるか？</param>
+		///<param name = "knockback">敵を押されるか？</param>
+		///<param name = "added position">前に動かせるため</param>
+		///<param name = "added size">サイズが変更できるため</param>
+		///<param name = "life span">何秒に実現させるのか？</param>
+		Laser(Math::Vector3 first_position, Math::Vector3 size, Side side, int attack, int knockdown, int knockback, Math::Vector3 added_position, Math::Vector3 added_size, float life_span_timer);
 		~Laser();
 		virtual std::vector < std::shared_ptr<Attack>>& Collide();
 		virtual void Update();
@@ -22,5 +31,7 @@ namespace AttackSystem
 
 	private :
 		Utility::Timer m_life_span_timer;
+		Math::Vector3 m_added_position;
+		Math::Vector3 m_added_size;
 	};
 }
