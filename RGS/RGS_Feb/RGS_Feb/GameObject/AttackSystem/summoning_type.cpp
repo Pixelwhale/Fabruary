35b6d8b31c65a4109e0d95905a04371da2188762
Adrian Rhesa <adrian.rhesa.ss@gmail.com>
2018-02-21 13:50:32 +0900

    SummoningTypeを追加する

---
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/summoning_type.cpp
@@ -0,0 +1,25 @@
+//-------------------------------------------------------
+// 作成者：シスワントレサ
+// 作成日：2018.02.21
+// 内容　：長くなるとか、大きくなるとか前に直線に行くスキル
+//-------------------------------------------------------
+
+#pragma once
+#include <GameObject\AttackSystem\summoning_type.h>
+
+using namespace AttackSystem;
+
+//コンストラクタ
+SummoningType::SummoningType(Math::Vector3 position, Math::Vector3 size, Side side, int attack, int knockdown, int knockback, float life_span_timer)
+	: Attack(position, size, side, attack, knockdown, knockback), m_life_span_timer(life_span_timer)
+{
+
+}
+
+//デストラクタ
+SummoningType::~SummoningType() {};
+
+std::vector<std::shared_ptr<Attack>>& Collide()
+{
+
+}
--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/summoning_type.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/summoning_type.h
@@ -1,34 +1,33 @@
 //-------------------------------------------------------
 // 作成者：シスワントレサ
-// 作成日：2018.02.15
-// 内容　：攻撃（キック）の当たり判定
+// 作成日：2018.02.21
+// 内容　：このスキルは攻撃ダメージが零だが、他のスキル玉を生成する。
 //-------------------------------------------------------
 
 #pragma once
-
 #include <GameObject\AttackSystem\attack.h>
 #include <Utility\timer.h>
 
 namespace AttackSystem
 {
-	class Kick : public Attack
+	class SummoningType : public Attack
 	{
 	public :
 		///<summary>コンストラクタ</summary>
+		///<param name = "side">キャラクターはどんなチームに属する</param>
 		///<param name = "position">キャラクターの位置</param>
 		///<param name = "size">攻撃の大きさ</param>
-		///<param name = "side">キャラクターはどんなチームに属する</param>
 		///<param name = "attack">攻撃ポイント</param>
 		///<param name = "knockdown">敵を倒せられるか？</param>
 		///<param name = "knockback">敵を押されるか？</param>
 		///<param name = "life span">何秒に実現させるのか？</param>
-		Kick(Math::Vector3 position, Math::Vector3 size, Side side, int attack, int knockdown, int knockback, float life_span_timer);
-		~Kick();
-		virtual std::vector<std::shared_ptr<Attack>>& Collide();
+		SummoningType(Math::Vector3 position, Math::Vector3 size, Side side, int attack, int knockdown, int knockback, float life_span_timer);
+		~SummoningType();
+		virtual std::vector <std::shared_ptr<Attack>>& Collide();
 		virtual void Update();
 		virtual void Draw();
 
-	private:
-		Utility::Timer m_life_span_timer; // まだあったているか？
+	private :
+		Utility::Timer m_life_span_timer;
 	};
 }
