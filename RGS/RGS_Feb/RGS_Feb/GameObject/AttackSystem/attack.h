9ddcde312abf06a6d1577287185efe5743f21534
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-02-21 11:32:05 +0900

    SetPositionとSetSizeメソードを消す

--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack.h
@@ -30,8 +30,6 @@ namespace AttackSystem
 		}
 
 		const Math::Vector3& GetPosition() const { return m_position; }
-		void SetPosition(Math::Vector3 position) { m_position = position; }
-		void SetSize(Math::Vector3 size) { m_size = size; }
 		Side GetSide() const { return m_side; }
 		int GetAtk() const { return m_atk; }
 		int GetKnockBack() const { return m_knockback; }
