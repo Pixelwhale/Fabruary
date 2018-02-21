0aa7546202e20cb00e8813393a9f5c1a60322c82
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-02-21 23:19:37 +0900

    バッグ解消

--- RGS/RGS_Feb/RGS_Feb/Math/collision_box.cpp
+++ RGS/RGS_Feb/RGS_Feb/Math/collision_box.cpp
@@ -14,21 +14,21 @@ bool CollisionBox::IsCollision(const CollisionBox& other) const
 
 bool CollisionBox::CheckX(const CollisionBox& other) const
 {
-	if (m_min.x <= other.m_min.x && m_max.x <= other.m_max.x) return true;
-	if (other.m_min.x <= m_min.x && other.m_max.x <= m_max.x) return true;
+	if (m_min.x <= other.m_min.x && m_max.x >= other.m_min.x) return true;
+	if (other.m_min.x <= m_min.x && other.m_max.x >= m_min.x) return true;
 	return false;
 }
 
 bool CollisionBox::CheckY(const CollisionBox& other) const
 {
-	if (m_min.y <= other.m_min.y && m_max.y <= other.m_max.y) return true;
-	if (other.m_min.y <= m_min.y && other.m_max.y <= m_max.y) return true;
+	if (m_min.y <= other.m_min.y && m_max.y >= other.m_min.y) return true;
+	if (other.m_min.y <= m_min.y && other.m_max.y >= m_min.y) return true;
 	return false;
 }
 
 bool CollisionBox::CheckZ(const CollisionBox& other) const
 {
-	if (m_min.z <= other.m_min.z && m_max.z <= other.m_max.z) return true;
-	if (other.m_min.z <= m_min.z && other.m_max.z <= m_max.z) return true;
+	if (m_min.z <= other.m_min.z && m_max.z >= other.m_min.z) return true;
+	if (other.m_min.z <= m_min.z && other.m_max.z >= m_min.z) return true;
 	return false;
 }
