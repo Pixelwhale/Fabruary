ada9d1b03fe280a1d92ca50cdcfc747775154c0c
リン <linjerry0906@gmail.com>
2018-02-21 17:51:24 +0900

    一番弱いAI

--- RGS/RGS_Feb/RGS_Feb/Scene/gameplay.cpp
+++ RGS/RGS_Feb/RGS_Feb/Scene/gameplay.cpp
@@ -27,15 +27,15 @@ void GamePlay::Initialize(SceneType previous)
 	m_character_manager->Add(Math::Vector3(), Side::kTeam1, make_shared<Character::KeyboardController>(), make_shared<Job::Programmer>(Side::kTeam1), m_attack_manager);
 
 	m_meta_ai = make_shared<AI::MetaAI>(m_character_manager, m_attack_manager);
-	m_meta_ai->AddCom(Math::Vector3(400, 0, 0), Side::kTeam2, make_shared<Job::Programmer>(Side::kTeam2), 9);
-	m_meta_ai->AddCom(Math::Vector3(-400, 0, 0), Side::kTeam3, make_shared<Job::Programmer>(Side::kTeam3), 9);
+	m_meta_ai->AddCom(Math::Vector3(400, 0, 0), Side::kTeam2, make_shared<Job::Programmer>(Side::kTeam2), 1);
+	m_meta_ai->AddCom(Math::Vector3(-400, 0, 0), Side::kTeam3, make_shared<Job::Programmer>(Side::kTeam3), 1);
 }
 
 void GamePlay::Update()
 {
 	m_meta_ai->Update();
 	m_character_manager->Update();
-	m_attack_manager->Update(m_character_manager);
+	//m_attack_manager->Update(m_character_manager);
 }
 
 void GamePlay::CheckEnd() 
