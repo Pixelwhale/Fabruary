0927822bb7864f874b2e82a5fc25c178fcc766bc
Adrian Rhesa <adrian.rhesa.ss@gmail.com>
2018-02-23 11:03:44 +0900

    OmniSlashクラスを追加

--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/omnislash.h
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/omnislash.h
@@ -1,20 +1,20 @@
 //-------------------------------------------------------
 // 作成者：シスワントレサ
-// 作成日：2018.02.15
-// 内容　：攻撃（キック）の当たり判定
+// 作成日：2018.02.23
+// 内容　：攻撃（オムニスラッシュ）の当たり判定
 //-------------------------------------------------------
 
 #pragma once
-
 #include <GameObject\AttackSystem\attack.h>
 #include <Utility\timer.h>
 
 namespace AttackSystem
 {
-	class Kick : public Attack
+	class OmniSlash : public Attack
 	{
 	public :
 		///<summary>コンストラクタ</summary>
+		///<param name = "side">キャラクターはどんなチームに属する</param>
 		///<param name = "position">キャラクターの位置</param>
 		///<param name = "size">攻撃の大きさ</param>
 		///<param name = "side">キャラクターはどんなチームに属する</param>
@@ -22,17 +22,20 @@ namespace AttackSystem
 		///<param name = "knockdown">敵を倒せられるか？</param>
 		///<param name = "knockback">敵を押されるか？</param>
 		///<param name = "life span">何秒に実現させるのか？</param>
-		Kick(Math::Vector3 position, Math::Vector3 size, Side side, int attack, int knockdown, int knockback, int dbreak, float life_span_timer);
-		~Kick();
-		virtual std::vector<std::shared_ptr<Attack>> Collide();
+		OmniSlash(Math::Vector3 position, Math::Vector3 size, Side side, int attack, int knockdown, int knockback, int dbreak, float life_span_timer);
+		~OmniSlash();
+		virtual std::vector < std::shared_ptr<Attack>> Collide();
 		virtual void Update();
 		virtual void Draw();
 
-	private:
+	private :
+		Math::Vector3 m_c_position;
+		Math::Vector3 m_c_size;
 		int m_c_attack;
 		int m_c_knockdown;
 		int m_c_knockback;
-		int m_c_break;
-		Utility::Timer m_life_span_timer; // まだあったているか？
+		int m_c_dbreak;
+		Utility::Timer m_life_span_timer;
 	};
 }
+
