1dd8f226ae7d4bb03826cf06d58582c8fa94e845
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-02-27 16:58:09 +0900

    最適化

--- RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack.cpp
+++ RGS/RGS_Feb/RGS_Feb/GameObject/AttackSystem/attack.cpp
@@ -10,16 +10,6 @@ using namespace Math;
 using namespace Size;
 
 Attack::Attack(Vector3 position, Vector3 size, Side side, int dmg, int knockback, int knockdown, int dbreak, int cool_down, Direction source_dir)
+	:m_position(position), m_size(size), m_side(side), m_knockback(knockback), m_knockdown(knockdown), m_break(dbreak), m_cool_down(cool_down), m_source_dir(source_dir),m_timer(0),m_is_end(false)
 {
-	m_position = position;
-	m_size = size;
-	m_side = side;
-	m_dmg = dmg;
-	m_knockback = knockback;
-	m_knockdown = knockdown;
-	m_break = dbreak;
-	m_cool_down = cool_down;
-	m_source_dir = source_dir;
-	m_timer = 0;
-	m_is_end = false;
 }
